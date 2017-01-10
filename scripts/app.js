1256e5e0b60c3d44e2c7772edf2476d4e72e1046
Hassan Mughal <hassan.mughal@gmail.com>
2017-01-10 18:10:53 +0300

    latest code

--- scripts/app.js
+++ scripts/app.js
@@ -4,11 +4,10 @@ var app = angular.module('app', ['ngRoute','app.directive.ngRepeatFinished']);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	$routeProvider
-
 			// route for the home page
-			.when('/', {
-				templateUrl : 'pages/home.html',
-				controller  : 'appController'
+			.when('/:serviceCat', {
+				templateUrl : 'pages/services.html',
+				controller  : 'appController' //to avoid calling twice
 			}) //remove semi-colon when extending routes
 
 			// route for the about page
@@ -17,64 +16,99 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 				controller  : 'customerController'
 			});
 
+			//$locationProvider.html5Mode(true); -- for pretty URLS
+
+
 		}]);
 
-app.controller('appController', ['$scope', '$location', function($scope, $location) {
+app.controller('appController', ['$scope', '$location', '$http','$routeParams', '$route', '$window', 
+		function($scope, $location, $http, $routeParams, $route, $window) {
 
-	var chartsData = [{
-		id: 'customer_1',
-		value: 'service1'
-	}, {
-		id: 'customer_2',
-		value: 'service2'
-	},{
-		id: 'customer_3',
-		value: 'service3'
-	}, {
-		id: 'customer_4',
-		value: 'service4'
-	},{
-		id: 'customer_5',
-		value: 'service5'
-	}, {
-		id: 'customer_6',
-		value: 'service6'
-	}];
+	//var serviceCategories;
+	//var servicesAlarms;
+	var serviceCat = $routeParams.serviceCat;
 
-	$scope.data = chartsData;
-			// Create the data table.
-			var chartData = new google.visualization.DataTable();
-			chartData.addColumn('string', 'Circuits');
-			chartData.addColumn('number', 'NumCircuits');
-			chartData.addRows([
-				['Outage', 3],
-				['Degradation', 1],
-				['Non-Service Affecting', 3]
-				]);
+	if (serviceCat==null || serviceCat=='All')
+		serviceCat = '*';
 
-		// Set chart options
-		var options = {
-			'title':'Service Name',
-			'width':320,
-			'height':260,
-			colors: ['red', 'orange', '#59b20a'],
-			pieHole: 0.4,
-			pieSliceTextStyle: {color: 'white', fontSize: '11'},
-			titleTextStyle: { color: '#007DB0', fontSize: '13'},
-			legend: {'position': 'none'},
+	//console.log(serviceCat);
 
-		};
 
-		$scope.$on('drawServiceCharts', function(ngRepeatFinishedEvent) {
+	$http.get('/json/service_level_alarms.json')
+	.success(function(data) {
+		//angular.extend(_this, data);
+		//========Need this to fetch data to call ng-repeat -- Need to replace multiple calls with factory =======//
+		//console.log(serviceCat);
+		$scope.data = jsonPath(data, "$.services." + serviceCat + ".*");
+		//console.log($scope.data);
+		//defer.resolve(); // Find out the reason for using
+	})
+	.error(function() {
+		console.log("Failed to fetch service alarms data.");
+		//defer.reject('could not find someFile.json');
+	});
 
+
+	 $scope.selectServiceType = function(serviceCat) {
+
+	 	//console.log(serviceCat);
+	 	$location.path('/' + serviceCat);
+	 	//$window.location.reload();
+		$route.reload();
+
+
+	 };
+
+
+
+	$scope.$on('drawServiceCharts', function(ngRepeatFinishedEvent) {
+
+		//console.log(ngRepeatFinishedEvent);
+
+		$http.get('/json/service_level_alarms.json')
+		.success(function(data) {
+			//angular.extend(_this, data);  <----
+			var chartsData = jsonPath(data, "$.services."+serviceCat+".*");
+			//console.log($scope.data);
+
+			// Set chart options
+			var options = {
+				'title':'Service Name',
+				'width':320,
+				'height':260,
+				colors: ['red', 'orange', '#59b20a'],
+				pieHole: 0.4,
+				pieSliceTextStyle: {color: 'white', fontSize: '11'},
+				titleTextStyle: { color: '#007DB0', fontSize: '13'},
+				legend: {'position': 'none'},
+
+			};
+
+			//alert(customerData);   <----
 			for (i=0; i <chartsData.length; i++){
-				var chart = new google.visualization.PieChart(document.getElementById(chartsData[i].id));
+				var chart = new google.visualization.PieChart(document.getElementById(chartsData[i].serviceType));
+
+		        var chartData = google.visualization.arrayToDataTable([
+		          ['Type', 'Count'],
+		          ['Outage', chartsData[i].alarmsSeverity1],
+		          ['Degradation', chartsData[i].alarmsSeverity2]
+		        ]);
+
 				chart.draw(chartData, options);
-			}
+			};
+
+			//defer.resolve(); // Find out the reason for using
+		})
+		.error(function() {
+			console.log("Failed to fetch data.");
+		//defer.reject('could not find someFile.json');
 		});
 
-		$scope.drawCustomerCharts = function(customerId){
+	});
 
+
+
+	$scope.drawCustomerCharts = function(customerId){
 			//alert(customerId);
 			$location.path('/customers/' + customerId);
 		}
@@ -87,24 +121,11 @@ app.controller('appController', ['$scope', '$location', function($scope, $locati
 app.controller('customerController', [ '$scope', '$location', '$routeParams', 
 	function($scope, $location, $routeParams) {
 
-	var chartsData = [{
-		id: 'circuit_1',
-		value: 'customer1'
-	}, {
-		id: 'circuit_2',
-		value: 'customer2'
-	},{
-		id: 'circuit_3',
-		value: 'customer3'
-	}, {
-		id: 'circuit_4',
-		value: 'customer4'
-	},{
-		id: 'circuit_5',
-		value: 'customer5'
-	}];
+		var chartsData = [{	id: 'circuit_1',value: 'customer1'}, { id: 'circuit_2',	value: 'customer2'},
+		{id: 'circuit_3',value: 'customer3'}, {	id: 'circuit_4',value: 'customer4'},
+		{id: 'circuit_5',value: 'customer5'	}];
 
-	$scope.data = chartsData;
+		$scope.data = chartsData;
 			// Create the data table.
 			var chartData = new google.visualization.DataTable();
 			chartData.addColumn('string', 'Circuits');
@@ -117,7 +138,7 @@ app.controller('customerController', [ '$scope', '$location', '$routeParams',
 
 		// Set chart options
 		var options = {
-			'title':'Customer Name',
+			'title':'Customer Name 1',
 			'width':320,
 			'height':260,
 			colors: ['red', 'orange', '#59b20a'],
@@ -129,7 +150,7 @@ app.controller('customerController', [ '$scope', '$location', '$routeParams',
 		};
 
 		$scope.$on('drawCustomerCharts', function(ngRepeatFinishedEvent) {
-			alert("Showing circuits for customer: " + $routeParams.customerId);
+			//alert("Showing circuits for customer: " + $routeParams.customerId);
 
 			for (i=0; i <chartsData.length; i++){
 				var chart = new google.visualization.PieChart(document.getElementById(chartsData[i].id));
@@ -138,7 +159,7 @@ app.controller('customerController', [ '$scope', '$location', '$routeParams',
 		});
 
 		$scope.drawCircuitCharts = function(circuitId){
-			alert(circuitId);
+			//alert(circuitId);
 
 		}
 
