00605d07001fbde3f48f6c6316bd21ed1fb42d0f
Hassan Latif <hassan.latif@gmail.com>
2017-04-20 08:46:19 +0300

    changed table location

--- sandbox/ngGoogleCharts.js
+++ sandbox/ngGoogleCharts.js
@@ -7,15 +7,37 @@ googleChart.directive("googleChart",function(){
         restrict : "A",
         link: function(scope, elem, attr){
 
-            //console.log("scope[attr.ngModel]", scope[attr.ngModel]);
+            // console.log("scope[attr.ngModel]", scope[attr.ngModel]);
 
-            var dt = scope[attr.ngModel]['table'];
+            var data = scope[attr.ngModel];
 
-            var options = {};
-                options.title = scope[attr.ngModel]['title'];
+            var dataTable = google.visualization.arrayToDataTable([
+                ['Type', 'Count'],
+                [data.badCircuits.toString(), data.badCircuits],
+                [data.goodCircuits.toString(), data.goodCircuits]
+                ]);
+
+            var options = {
+                'width':290,
+                'height':209,
+                colors: ['red', '#59b20a'],
+                pieHole: 0.4,
+                pieSliceText: 'percentage',
+                sliceVisibilityThreshold: 0.0001,
+                pieSliceTextStyle: {color: 'Black', fontSize: '12', bold: true},
+                titleTextStyle: { color: '#007DB0', fontSize: '13'},
+                legend: {'position': 'bottom'},
+                slices: { 1: {offset: 0.05}},
+                tooltip: {
+                  showColorCode: true,
+                  text: 'percentage'
+                }
+            };
+                            
+            options.title = data.customerName;
 
             var googleChart = new google.visualization[attr.googleChart](elem[0]);
-             googleChart.draw(dt,options)
+            googleChart.draw(dataTable,options)
         }
     }
 });
